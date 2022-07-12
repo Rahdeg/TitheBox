2b912cdca32ef9afbbfcf4672e11cdbe29511b9c
Rahdeg <walett95@gmail.com>
2022-07-12 16:58:22 +0100

    auth

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {signUp, signIn,update} = require("../controllers/user.controllers");
 const {signUpValidation, signInValidation,updateValidation} = require("../validation/user.validations");
-
+// const Authorization = require('../middlewares/authorization');
 router.post('/signUp', signUpValidation, signUp);
 router.post('/signIn', signInValidation, signIn);
 router.put('/update/:id', updateValidation, update);
