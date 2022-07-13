93320555c859ef7ce88f28c98f05543e2a4d03f3
Rahdeg <walett95@gmail.com>
2022-07-13 02:47:36 +0100

    ss

--- src/verification/nodemailer.js
+++ src/verification/nodemailer.js
@@ -4,7 +4,7 @@ const transporter = ()=>nodemailer.createTransport({
     service:'gmail',
     auth: {
         user:"walett95@gmail.com",
-        pass:"gmmxydmtwapciucf"
+        pass:"ekojbojugklbrweo"
     }
 });
 
