3e0a4ac02d346f13ac60b8ee7db807573be5761c
Rahdeg <walett95@gmail.com>
2022-07-26 08:39:27 +0100

    get bank

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -162,17 +162,19 @@ exports. updateSubaccount = async function (data) {
 }
 
 
+
+
+
 exports. getbankcode = async function(){
-var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': '{{BASE_API_URL}}/banks/NG',
-  'headers': {
-    'Authorization': 'Bearer FLUTTER_SEC'
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+    try {
+        const payload = {
+            
+            "country":"NG" //Pass either NG, GH, KE, UG, ZA or TZ to get list of banks in Nigeria, Ghana, Kenya, Uganda, South Africa or Tanzania respectively
+            
+        }
+        const response = await flw.Bank.country(payload)
+        console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
 }
