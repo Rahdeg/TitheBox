d026ace14d84b45697ee90b5712acb97bbaecb38
Rahdeg <walett95@gmail.com>
2023-01-25 21:29:57 +0100

    change enviroment

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ require("dotenv").config()
 const port = process.env.PORT || 5000;
 const connectDatabase = async function(app){
     try {
-        mongoose.connect(process.env.DB_URI, ()=>{
+        mongoose.set('strictQuery', false);
+        mongoose.connect(process.env.DB_URI,{dbName: process.env.DB_NAME}, ()=>{
             console.log("Database Connected");
             app.listen(port, ()=>{
                 console.log(`Server listening on ${port}`);
